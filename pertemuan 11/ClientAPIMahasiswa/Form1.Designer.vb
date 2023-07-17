51c733960fcaf65a89a6b8334ea7e2ade0fb0f89
FaqihWirahadi <103120110+FaqihWirahadi@users.noreply.github.com>
2023-07-17 13:27:09 +0700

    Add files via upload

---
+++ pertemuan 11/ClientAPIMahasiswa/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.txtNim = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.txtjk = New System.Windows.Forms.TextBox()
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(27, 85)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(0, 18)
+        Me.LinkLabel1.TabIndex = 29
+        '
+        'txtNim
+        '
+        Me.txtNim.Location = New System.Drawing.Point(344, 49)
+        Me.txtNim.Margin = New System.Windows.Forms.Padding(10, 7, 10, 7)
+        Me.txtNim.Name = "txtNim"
+        Me.txtNim.Size = New System.Drawing.Size(357, 25)
+        Me.txtNim.TabIndex = 25
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(71, 183)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 18)
+        Me.Label4.TabIndex = 33
+        Me.Label4.Text = "Prodi"
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Location = New System.Drawing.Point(349, 173)
+        Me.txtProdi.Margin = New System.Windows.Forms.Padding(10, 7, 10, 7)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(354, 25)
+        Me.txtProdi.TabIndex = 28
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(71, 44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 18)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Nim"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(784, 44)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 32)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Simpan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(71, 131)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 18)
+        Me.Label3.TabIndex = 32
+        Me.Label3.Text = "Jk"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(71, 85)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 18)
+        Me.Label2.TabIndex = 31
+        Me.Label2.Text = "Nama"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(784, 85)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(124, 32)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtNama
+        '
+        Me.txtNama.Location = New System.Drawing.Point(344, 90)
+        Me.txtNama.Margin = New System.Windows.Forms.Padding(10, 7, 10, 7)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(357, 25)
+        Me.txtNama.TabIndex = 26
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(784, 125)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(124, 32)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Hapus"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'txtjk
+        '
+        Me.txtjk.Location = New System.Drawing.Point(349, 131)
+        Me.txtjk.Margin = New System.Windows.Forms.Padding(10, 7, 10, 7)
+        Me.txtjk.Name = "txtjk"
+        Me.txtjk.Size = New System.Drawing.Size(354, 25)
+        Me.txtjk.TabIndex = 27
+        '
+        'dgvData
+        '
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(50, 227)
+        Me.dgvData.Margin = New System.Windows.Forms.Padding(10, 7, 10, 7)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.Size = New System.Drawing.Size(877, 356)
+        Me.dgvData.TabIndex = 34
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(974, 623)
+        Me.Controls.Add(Me.dgvData)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.txtjk)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.txtNim)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("News706 BT", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents txtNim As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents txtjk As TextBox
+    Friend WithEvents dgvData As DataGridView
+End Class
